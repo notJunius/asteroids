0af060a892283bb8954dd5b6a24d8d7501976fdc
notJunius <120129256+notJunius@users.noreply.github.com>
2025-05-25 10:32:01 -0600

    added player, errors show but still runs

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = clock.tick(60) / 1000
+    main_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
+        updateable.update(dt)
         screen.fill("black")
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
 
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        clock.tick(60)
 
 
 if __name__ == "__main__":
